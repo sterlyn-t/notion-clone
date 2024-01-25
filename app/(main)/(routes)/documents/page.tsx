cf301e61b011ae5ec48d39926e144bb691dcd02f
Sterlyn <sterlyntang31@gmail.com>
2024-01-25 16:15:52 -0500

    Feat: Empty state documents screen

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,37 @@
+"use client";
 import React from "react";
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
 
 const DocumentsPage = () => {
-  return <div>DocumentsPage</div>;
+  const { user } = useUser();
+  return (
+    <div className="h-full items-center justify-center space-y-4 flex flex-col">
+      <Image
+        src="/empty.png"
+        alt="Empty"
+        className="dark:hidden"
+        height="300"
+        width="300"
+      />
+      <Image
+        src="/empty-dark.png"
+        alt="Empty"
+        className="dark:block hidden"
+        height="300"
+        width="300"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Jotion
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
 };
 
 export default DocumentsPage;
