71e8841f113e8a514bfb99c66bdbe00038b854d1
Sterlyn <sterlyntang31@gmail.com>
2024-01-25 02:13:21 -0500

    Feat: Adding theme provider + dark mode

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -11,7 +11,20 @@ const font = Poppins({
 const Logo = () => {
   return (
     <div className="hidden md:flex items-center gap-x-2">
-      <Image src="/logo.svg" alt="logo" height="40" width="40" />
+      <Image
+        src="/logo.svg"
+        alt="logo"
+        height="40"
+        width="40"
+        className="dark:hidden"
+      />
+      <Image
+        src="/logo-dark.svg"
+        alt="logo"
+        height="40"
+        width="40"
+        className="hidden dark:block"
+      />
       <p className={cn("font-semibold", font.className)}>Jotion</p>
     </div>
   );
