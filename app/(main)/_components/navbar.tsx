ada19e9104c7192d3ecd9e5dc03477b9898fd9d2
Sterlyn <sterlyntang31@gmail.com>
2024-01-27 16:56:33 -0500

    Feat: Banner for deleted/archived documents

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -6,6 +6,7 @@ import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import React from "react";
 import Title from "./title";
+import Banner from "./banner";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -45,6 +46,7 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
           <Title initialData={document} />
         </div>
       </nav>
+      {document.isArchived && <Banner documentId={document._id} />}
     </>
   );
 };
