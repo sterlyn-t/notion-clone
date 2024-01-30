80d191afc6aaa27d52e5714e32dc876ee3e71029
Sterlyn <sterlyntang31@gmail.com>
2024-01-29 23:52:11 -0500

    Feat: React dropzone for file upload

--- components/modals/cover-image-modal.tsx
+++ components/modals/cover-image-modal.tsx
@@ -1,18 +1,59 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Dialog, DialogContent, DialogHeader } from "../ui/dialog";
 import { useCoverImage } from "@/hooks/use-cover-image";
+import { SingleImageDropzone } from "../single-image-dropzone";
+import { useEdgeStore } from "@/lib/edgestore";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useParams } from "next/navigation";
+import { Id } from "@/convex/_generated/dataModel";
 
 const CoverImageModal = () => {
+  const [file, setFile] = useState<File>();
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const coverImage = useCoverImage();
+  const { edgestore } = useEdgeStore();
+  const update = useMutation(api.documents.update);
+  const params = useParams();
+
+  const onChange = async (file?: File) => {
+    if (file) {
+      setIsSubmitting(true);
+      setFile(file);
+
+      const res = await edgestore.publicFiles.upload({
+        file,
+      });
+
+      await update({
+        id: params.documentId as Id<"documents">,
+        coverImage: res.url,
+      });
+
+      onClose();
+    }
+  };
+
+  const onClose = () => {
+    setFile(undefined);
+    setIsSubmitting(false);
+    coverImage.onClose();
+  };
+
   return (
     <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
       <DialogContent>
         <DialogHeader>
           <h2 className="text-center text-lg font-semibold">Cover Image</h2>
         </DialogHeader>
-        <div>TODO: Upload image</div>
+        <SingleImageDropzone
+          className="w-full outline-none"
+          disabled={isSubmitting}
+          value={file}
+          onChange={onChange}
+        />
       </DialogContent>
     </Dialog>
   );
